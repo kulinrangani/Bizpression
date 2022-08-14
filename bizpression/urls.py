bede79ff938a8e6878956ddec809b295c7b34b26
kulin <kulinrangani01@gmail.com>
2022-08-14 13:35:59 +0530

    Updated url.py added static url

--- bizpression/urls.py
+++ bizpression/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('',include('pages.urls')),
-    path('shop/',include('shop.urls')),
-    path('contact/',include('contacts.urls')),
-    #path('accounts/',include('accounts.urls')),
+    path('', include('pages.urls')),
+    path('shop/', include('shop.urls')),
+    path('contact/', include('contacts.urls')),
+    # path('accounts/',include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# linking static files
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
